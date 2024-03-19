ed049e2b1d1ae37f081ea5c6d6819e1b0b6be645
zn0622zn <163973845+zn0622zn@users.noreply.github.com>
2024-03-19 18:57:54 +0800

    Delete shop-front-manager directory

--- shop-front-manager/src/libs/validate/base-rule.js
+++
@@ -1,8 +0,0 @@
-export const validateTel = (rule, value, callback) => {
-  var telRegex = /^\d{3,4}-\d{7,8}$/
-  if (value != '' && !telRegex.test(value)) {
-    callback(new Error('请输入正确的电话号码'))
-  } else {
-    callback()
-  }
-}
